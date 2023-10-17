0492cf96e15e6d90ef6d1e403bab9c90f60253c0
me_madness <p_rangel@abv.bg>
2023-10-17 18:00:43 +0200

    01.1

--- 01. black_flag.py
+++ 01. black_flag.py
@@ -8,4 +8,14 @@ for day in range(1, days + 1):
     total_plunder += daily_plunder
     
     if day % 3 == 0:
-        total_plunder
+        total_plunder += 0.5 * daily_plunder
+        
+    if day % 5 == 0:
+        total_plunder *= 0.7
+        
+        
+if total_plunder >= expected_plunder:       
+    print(f"Ahoy! {total_plunder:.2f} plunder gained")
+else:
+    percentage = (total_plunder)
+    print(f"Collected only {}% of the plunder")
